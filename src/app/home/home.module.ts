b2401ee2247535903625fad4dae81931cb5a491b
damoqiongqiu <damoqiongqiu@126.com>
2017-08-17 17:48:02 +0800

    嵌套路由

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { homeRoutes } from './home.routes';
+import { PictureComponent } from './picture/picture.component';
+import { TextComponent } from './text/text.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    PictureComponent,
+    TextComponent
   ],
   imports: [
     RouterModule.forChild(homeRoutes)
--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -1,9 +1,26 @@
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { PictureComponent } from './picture/picture.component';
+import { TextComponent } from './text/text.component';
 
 export const homeRoutes=[
     {
 		path:'',
-        component:HomeComponent
+        component:HomeComponent,
+        children:[
+            {
+                path:'',
+                redirectTo:'pictures',
+                pathMatch:'full'
+            },
+            {
+                path:'pictures',
+                component:PictureComponent
+            },
+            {
+                path:'text',
+                component:TextComponent
+            }
+        ]
 	}
 ];
